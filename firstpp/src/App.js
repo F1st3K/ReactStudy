aa0ad2af690614965153df9d5b56338c38896ca2
F1st3K <kostinnkita988@gmail.com>
2022-10-14 13:48:06 +0300

    first commit

---
+++ firstpp/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+           -_-
+        </p>
+        <a
+          className="App-link"
+          href="https://vk.me/join/qdovKZeskHdr/r3Sc1GvHfkd9YgLlEfGDz8="
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          go react)))
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
